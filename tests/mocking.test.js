6ba7c9f772d6620bcefac1056ec09d958ed955c9
Dariusz Okoński <darek200180@gmail.com>
2025-01-06 14:27:51 +0100

    add tests for isOnline

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -3,6 +3,7 @@ import { getExchangeRate } from '../src/libs/currency';
 import {
   getPriceInCurrency,
   getShippingInfo,
+  isOnline,
   login,
   renderPage,
   signUp,
@@ -152,5 +153,23 @@ describe('login', () => {
     const securityCode = spy.mock.results[0].value.toString();
 
     expect(sendEmail).toHaveBeenCalledWith(email, securityCode);
+  });
+});
+
+describe('isOnline', () => {
+  it('should return false if current hour is outside opening hours', () => {
+    vi.setSystemTime('2024-01-01 07:59');
+    expect(isOnline()).toBe(false);
+
+    vi.setSystemTime('2024-01-01 20:01');
+    expect(isOnline()).toBe(false);
+  });
+
+  it('should return true if current hour is within opening hours', () => {
+    vi.setSystemTime('2024-01-01 08:00');
+    expect(isOnline()).toBe(true);
+
+    vi.setSystemTime('2024-01-01 19:59');
+    expect(isOnline()).toBe(true);
   });
 });
