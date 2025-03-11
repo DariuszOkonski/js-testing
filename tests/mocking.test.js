f877e35e71393c6c8e2719f1a0efc619acdf22ae
Dariusz Okoński <darek200180@gmail.com>
2025-03-11 16:38:21 +0100

    add test suite

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -3,8 +3,15 @@ import { vi, it, expect, describe } from 'vitest';
 describe('test suite', () => {
   it('test case', () => {
     const greet = vi.fn();
-    greet();
 
-    expect(greet).toHaveBeenCalled();
+    // greet.mockReturnValue('Hello');
+    // greet.mockResolvedValue('Hello');
+    greet.mockImplementation((name) => 'Hello ' + name);
+
+    const result = greet('Mosh');
+
+    expect(greet).toHaveBeenCalledOnce();
+    expect(greet).toHaveBeenCalledWith('Mosh');
+    expect(result).toMatch(/hello mosh/i);
   });
 });
