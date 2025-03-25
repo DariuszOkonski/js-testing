215a8d2f85019566b44e410a021cc88815fc9f2e
Dariusz Okoński <darek200180@gmail.com>
2025-03-25 12:18:08 +0100

    add tests for login

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -2,6 +2,7 @@ import { vi, it, expect, describe } from 'vitest';
 import {
   getPriceInCurrency,
   getShippingInfo,
+  login,
   renderPage,
   signUp,
   submitOrder,
@@ -11,6 +12,7 @@ import { getShippingQuote } from '../src/libs/shipping';
 import { trackPageView } from '../src/libs/analytics';
 import { charge } from '../src/libs/payment';
 import { sendEmail } from '../src/libs/email';
+import security from '../src/libs/security';
 
 vi.mock('../src/libs/currency.js');
 vi.mock('../src/libs/shipping.js');
@@ -217,5 +219,17 @@ describe('signUp V1', () => {
     const args = vi.mocked(sendEmail).mock.calls[0];
     expect(args[0]).toBe(email);
     expect(args[1]).toMatch(/welcome/i);
+  });
+});
+
+describe('login', () => {
+  it('should email the one-time login code', async () => {
+    const email = 'name@domain.com';
+    const spy = vi.spyOn(security, 'generateCode');
+
+    await login(email);
+    const securityCode = spy.mock.results[0].value.toString();
+
+    expect(sendEmail).toHaveBeenCalledWith(email, securityCode);
   });
 });
